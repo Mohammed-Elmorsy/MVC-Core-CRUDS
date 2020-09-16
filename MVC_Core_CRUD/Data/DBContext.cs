7bbe4b3149989e6c5b552b16028c6bedb9a25a66
Mohammed Elmorsy <mohammed.elsayed.elmorsy>
2020-09-16 03:42:16 +0200

    first commit

---
+++ MVC_Core_CRUD/Data/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Core_CRUD.Models;
+
+namespace MVC_Core_CRUD.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext (DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Core_CRUD.Models.Student> Student { get; set; }
+    }
+}
